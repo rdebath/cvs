409b0b33aa85e945ef1b4397ce232bcfdf31b86c
Jim Blandy <jimb@floss.cyclic.com>
1995-07-27 20:30:33 +0000

    CVS-ID: windows-NT/ChangeLog 1.1 CVS-ID: windows-NT/alloca.h 1.1 CVS-ID: windows-NT/config.h 1.1 CVS-ID: windows-NT/options.h 1.1 CVS-ID: windows-NT/pwd.h 1.1

---
+++ windows-NT/pwd.h
@@ -0,0 +1,4 @@
+/* pwd.h --- Windows NT version of the password library routine header file
+   Jim Blandy <jimb@cyclic.com> --- July 1995  */
+
+/* nothing to put here yet.  We'll see what we need.  */
