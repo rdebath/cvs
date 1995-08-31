aa35bdead95c9ff5bec0366f37f6706673810ea3
Jim Blandy <jimb@floss.cyclic.com>
1995-08-31 00:15:14 +0000

    * acconfig.h (CVS_SUPPORT, CLIENT_SUPPORT, SERVER_SUPPORT): New symbols, which autoheader will use to build config.h.in from configure.in. * configure.in (SERVER_SUPPORT, CLIENT_SUPPORT): Remove spaces between AC_DEFINEs and opening parens of argument lists.  Oops. * configure: Rebuild using autoconf. * config.h.in: Rebuild using autoheader.
    CVS-ID: acconfig.h 1.2
    CVS-ID: config.h.in 1.9
    CVS-ID: configure 1.18
    CVS-ID: configure.in 1.18

--- acconfig.h
+++ acconfig.h
@@ -1,2 +1,13 @@
 /* Define if you have MIT Kerberos version 4 available.  */
 #undef HAVE_KERBEROS
+
+/* This is always defined; it tells some library files to include
+   CVS-specific features.  */
+#undef CVS_SUPPORT
+
+/* Define if you want CVS to be able to be a remote repository client.  */
+#undef CLIENT_SUPPORT
+
+/* Define if you want CVS to be able to serve repositories to remote
+   clients.  */
+#undef SERVER_SUPPORT
