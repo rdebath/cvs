b8f2a7671b3cff76de3113e3642fc1a9c524678a
Jim Blandy <jimb@floss.cyclic.com>
1995-09-20 19:34:59 +0000

    * configure.in: #define the symbols DIFF and GREP to be the paths to the DIFF and GREP programs; their values will be edited into src/options.h (and config.h, coincidentally). * acconfig.h (DIFF, GREP): Add these. * configure, config.h.in: Rebuilt using autoconf and autoheader.
    CVS-ID: acconfig.h 1.3
    CVS-ID: config.h.in 1.10
    CVS-ID: configure 1.20
    CVS-ID: configure.in 1.20

--- acconfig.h
+++ acconfig.h
@@ -11,3 +11,9 @@
 /* Define if you want CVS to be able to serve repositories to remote
    clients.  */
 #undef SERVER_SUPPORT
+
+/* the path to the gnu diff program on your system  */
+#undef DIFF
+
+/* the path to the gnu grep program on your system  */
+#undef GREP
