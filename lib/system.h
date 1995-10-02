0ede224da4a02a3bde39c2f219b0baaf57756dff
Jim Blandy <jimb@floss.cyclic.com>
1995-10-02 23:11:39 +0000

    * system.h: Doc fix.
    CVS-ID: lib/system.h 1.9

--- lib/system.h
+++ lib/system.h
@@ -344,11 +344,11 @@ char *getwd ();
 #define CVS_MKDIR mkdir
 #endif
 
-/* Some file systems are case-insensitive.  If FOLD_FN_CHAR is #defined, it maps
-   the character C onto its "canonical" form.  In a case-insensitive system,
-   it would map all alphanumeric characters to lower case.  Under Windows NT,
-   / and \ are both path component separators, so FOLD_FN_CHAR would map them both
-   to /.  */
+/* Some file systems are case-insensitive.  If FOLD_FN_CHAR is
+   #defined, it maps the character C onto its "canonical" form.  In a
+   case-insensitive system, it would map all alphanumeric characters
+   to lower case.  Under Windows NT, / and \ are both path component
+   separators, so FOLD_FN_CHAR would map them both to /.  */
 #ifndef FOLD_FN_CHAR
 #define FOLD_FN_CHAR(c) (c)
 #define fnfold(filename) (filename)
