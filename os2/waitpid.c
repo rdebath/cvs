a965b534cb0d79ec7abfd37901704f83c99be678
Karl Fogel <kfogel@red-bean.com>
1995-12-14 01:32:15 +0000

    waitpid.c (waitpid): don't treat statusp specially.
    CVS-ID: os2/ChangeLog 1.83
    CVS-ID: os2/waitpid.c 1.5

--- os2/waitpid.c
+++ os2/waitpid.c
@@ -15,24 +15,13 @@ pid_t waitpid (pid, statusp, options)
      int *statusp;
      int options;
 {
-  int local_statusp;
   pid_t rc;
 
   /* We don't know how to deal with any options yet.  */
   assert (options == 0);
   
-  rc = _cwait (&local_statusp, pid, WAIT_CHILD);
+  rc = _cwait (statusp, pid, WAIT_CHILD);
   
-  /* 
-   * We only want the least-significant byte of the status, since the
-   * rest of it may be operating-system dependent.  We'll know if the
-   * child exited normally, but not whether it exited with status 0.
-   * Since gzip appears to exit with status 1 even after everything
-   * goes right, this is how we deal.  Ick.
-   */
-  if (statusp)
-	  *statusp = (char) local_statusp;
-
   if (rc == -1)
     {
       if (errno == ECHILD)
