f2a1c4df35c372caf5971c0c0697ffd249081744
Karl Fogel <kfogel@red-bean.com>
1995-12-18 22:51:00 +0000

    Removed debugging gunk.
    CVS-ID: os2/ChangeLog 1.93
    CVS-ID: os2/getpass.c 1.2

--- os2/getpass.c
+++ os2/getpass.c
@@ -2,31 +2,24 @@
 #include <string.h>
 #include "cvs.h"
 
-/* This is for debugging on beezley. */
-/* #define HARDCODED_PASSWD "" */
-#undef HARDCODED_PASSWD
-
 char *
 getpass (char *prompt)
 {
-#ifndef HARDCODED_PASSWD
-	static char passbuf[1024];
-	char *p;
+    static char passbuf[30];
+    int i;
+    char *p;
+    int ch;
+    
+    printf ("%s", prompt);
+    fflush (stdout);
+    
+    p = passbuf, i = 0;
+    while (((ch = getchar ()) != '\n') && (ch != EOF))
+    {
+        if (i++ < 24)
+            *p++ = ch;
+    }
+    *p = '\0';
 
-	printf ("%s", prompt);
-	fflush (stdout);
-
-	fgets (passbuf, 1023, stdin);
-	p = strchr (passbuf, '\n');
-
-	if (p == NULL)
-		error (1, 0, "password oddity -- where is the newline?");
-
-	*p = '\0';
-	return passbuf;
-#else /* HARDCODED_PASSWD */
-	printf ("Sending hardcoded password.\n");
-	fflush (stdout);
-	return HARDCODED_PASSWD;
-#endif /* ! HARDCODED_PASSWD */
+    return passbuf;
 }
