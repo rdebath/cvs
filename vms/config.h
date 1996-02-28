6f51b599a14f7d1193d0733633feda098e677326
Jim Kingdon <kingdon@redhat.com>
1996-02-28 16:30:35 +0000

    * config.h: Remove C_ALLOCA, CRAY_STACKSEG_END, HAVE_ALLOCA, 	HAVE_ALLOCA_H, and STACK_DIRECTION to reflect alloca removal.
    CVS-ID: os2/ChangeLog 1.126
    CVS-ID: os2/config.h 1.23
    CVS-ID: vms/ChangeLog 1.4
    CVS-ID: vms/config.h 1.2

--- vms/config.h
+++ vms/config.h
@@ -15,9 +15,6 @@
 /* Only good for NT or DOS with hacked open */
 /* #undef LINES_CRLF_TERMINATED */
 
-/* Define if using alloca.c.  */
-#define C_ALLOCA 1
-
 /* Define if type char is unsigned and you are not using gcc.  */
 #ifndef __CHAR_UNSIGNED__
 /* #undef __CHAR_UNSIGNED__ */
@@ -25,10 +22,6 @@
 
 /* Define to empty if the keyword does not work.  */
 /* #undef const */
-
-/* Define to one of _getb67, GETB67, getb67 for Cray-2 and Cray-YMP systems.
-   This function is required for alloca.c support on those systems.  */
-/* #undef CRAY_STACKSEG_END */
 
 /* Define if you have <dirent.h>.  */
 /* #undef DIRENT */
@@ -38,12 +31,6 @@
 
 /* Define to `int' if <sys/types.h> doesn't define.  */
 /* #undef gid_t */
-
-/* Define if you have alloca, as a function or macro.  */
-/* #undef HAVE_ALLOCA */
-
-/* Define if you have <alloca.h> and it should be used (not on Ultrix).  */
-/* #undef HAVE_ALLOCA_H */
 
 /* Define if you support file names longer than 14 characters.  */
 /* #undef HAVE_LONG_FILE_NAMES */
@@ -79,15 +66,6 @@
 
 /* Define to `unsigned' if <sys/types.h> doesn't define.  */
 /* #undef size_t */
-
-/* If using the C implementation of alloca, define if you know the
-   direction of stack growth for your system; otherwise it will be
-   automatically deduced at run-time.
-	STACK_DIRECTION > 0 => grows toward higher addresses
-	STACK_DIRECTION < 0 => grows toward lower addresses
-	STACK_DIRECTION = 0 => direction of growth unknown
- */
-/* #undef STACK_DIRECTION */
 
 /* Define if you have the ANSI C header files.  */
 #define STDC_HEADERS 1
