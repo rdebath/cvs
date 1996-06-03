ec969a37b90d7bfd42eed4f16cf7ca33ee67cbd1
Noel Cragg <noel@shave.cnet.com>
1996-06-03 09:21:57 +0000

    * version.c: version 1.8.3.
    CVS-ID: src/ChangeLog 1.606
    CVS-ID: src/version.c 1.40

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.8.2";
+char *version_string = "\nConcurrent Versions System (CVS) 1.8.3";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
