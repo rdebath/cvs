c45ff6e28d3646f0784731fd276e657db9593a7e
Jim Kingdon <kingdon@redhat.com>
1996-07-04 04:02:25 +0000

    * version.c: Version 1.8.5.
    CVS-ID: src/version.c 1.42

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.8.4";
+char *version_string = "\nConcurrent Versions System (CVS) 1.8.5";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
