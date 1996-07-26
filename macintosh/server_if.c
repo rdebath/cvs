432f9c0780207e9c2fe5a859e2775ba7f0218c40
Jim Kingdon <kingdon@redhat.com>
1996-07-26 21:53:30 +0000

    * server_if.c: Revert to version before Ladwig's 26 Jul 96 	submission.  In particular, this is to pick up the following 	changes:
    	Tue Mar 19 17:51:52 1996  Norbert Kiesel  <nk@col.sw-ley.de>
    
    	* server_if.c (macos_start_server): Added support for
    	  CVS_RCMD_PORT (and thus made consistent with WindowsNT and VMS)
    
    	Wed Feb 28 11:31:11 1996  Jim Kingdon  <kingdon@harvey.cyclic.com>
    
    	* server_if.c (macos_start_server): Don't use alloca.
    
    CVS-ID: macintosh/ChangeLog 1.12
    CVS-ID: macintosh/server_if.c 1.6

--- macintosh/server_if.c
+++ macintosh/server_if.c
@@ -10,50 +10,54 @@
 #include <GUSI.h>
 #include <sys/socket.h>
 
-#ifdef __POWERPC__
-#include <alloca.h>
-#endif
-
 static int read_fd, write_fd;
     
 void
-macos_start_server (	int *tofd,
-							int *fromfd,
-							char *client_user,
-							char *server_user,
-							char *server_host,
-							char *server_cvsroot)
+macos_start_server (int *tofd, int *fromfd,
+		  char *client_user,
+		  char *server_user,
+		  char *server_host,
+		  char *server_cvsroot)
 {
-	char					*cvs_server;
-	char					command[1024];
-	struct servent 	*s;
-	unsigned short		port;
+    char *cvs_server;
+    char *command;
+    char *portenv;
+    struct servent *sptr;
+    unsigned short port;
 
-	if (! (cvs_server = getenv ("CVS_SERVER")))
-		cvs_server = "cvs";
-	sprintf (command, "%s -d %s server", cvs_server, server_cvsroot);
+    if (! (cvs_server = getenv ("CVS_SERVER")))
+        cvs_server = "cvs";
+    command = xmalloc (strlen (cvs_server)
+		       + strlen (server_cvsroot)
+		       + 50);
+    sprintf (command, "%s -d %s server", cvs_server, server_cvsroot);
 
-	if ((s = getservbyname("shell", "tcp")) == NULL)
-    	port = 514; /* error (1, errno, "cannot getservbyname for shell, tcp"); */
-	else
-		port = s->s_port;
-		
-	read_fd = rcmd(	&server_host,
-							port,
-							client_user,
-							(server_user ? server_user : client_user),
-							command,
-							0);
-	if (read_fd < 0) error (1, errno, "cannot start server via rcmd");
+    portenv = getenv("CVS_RCMD_PORT");
+    if (portenv)
+	port = atoi(portenv);
+    else if ((sptr = getservbyname("shell", "tcp")) != NULL)
+	port = sptr->s_port;
+    else
+	error (1, errno, "cannot getservbyname for shell, tcp");
+
+    read_fd = rcmd (&server_host,
+    	            port,
+    	            client_user,
+	            (server_user ? server_user : client_user),
+	            command,
+	            0);
+    if (read_fd < 0)
+	error (1, errno, "cannot start server via rcmd");
     
-	/* Split the socket into a reading and a writing half.  */
-
-	if ((write_fd = dup (read_fd)) < 0)
-		error (1, errno, "duplicating server connection");
+    /* Split the socket into a reading and a writing half.  */
+    if ((write_fd = dup (read_fd)) < 0)
+        error (1, errno, "duplicating server connection");
     
-	*tofd = write_fd;
-	*fromfd = read_fd;
+    *tofd = write_fd;
+    *fromfd = read_fd;
+    free (command);
 }
+
 
 void
 macos_shutdown_server (int to_server)
