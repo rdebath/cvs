c8bfdfb68555cc4b55c16031aa688f9fcc1c587d
Jim Kingdon <kingdon@redhat.com>
1996-09-24 19:35:16 +0000

    * options.h: Add TMPDIR_DFLT.
    CVS-ID: macintosh/ChangeLog 1.26
    CVS-ID: macintosh/options.h 1.6
    CVS-ID: os2/ChangeLog 1.139
    CVS-ID: os2/options.h 1.16
    CVS-ID: vms/ChangeLog 1.20
    CVS-ID: vms/options.h 1.5
    CVS-ID: windows-NT/ChangeLog 1.56
    CVS-ID: windows-NT/options.h 1.15

--- vms/options.h
+++ vms/options.h
@@ -118,6 +118,20 @@
 #endif
 
 /*
+ * The password-authenticating server creates a temporary checkout of
+ * the affected files.  The variable TMPDIR_DFLT (or even better, the
+ * command-line option "-T" in the line for CVS in /etc/inetd.conf)
+ * can be used to specify the used directory.  This directory will
+ * also be used for other temporary files.
+ *
+ * Is there a better default for VMS than SYS$LOGIN?  I'm not aware of a
+ * standard convention on VMS.
+ */
+#ifndef TMPDIR_DFLT
+#define	TMPDIR_DFLT	"sys$login"
+#endif
+
+/*
  * The default editor to use, if one does not specify the "-e" option to cvs,
  * or does not have an EDITOR environment variable.  I set this to just "vi",
  * and use the shell to find where "vi" actually is.  This allows sites with
