26aa839263349d81f3373197900b4bb668eaeb1f
Jim Blandy <jimb@floss.cyclic.com>
1996-09-25 23:51:45 +0000

    * vasprintf.c: Fix type clashes in calls to strtoul.
    CVS-ID: lib/vasprintf.c 1.7

--- lib/vasprintf.c
+++ lib/vasprintf.c
@@ -69,7 +69,7 @@ int_vasprintf (result, format, args)
 	      total_width += abs (va_arg (ap, int));
 	    }
 	  else
-	    total_width += strtoul (p, &p, 10);
+	    total_width += strtoul (p, (char **)&p, 10);
 	  if (*p == '.')
 	    {
 	      ++p;
@@ -79,7 +79,7 @@ int_vasprintf (result, format, args)
 		  total_width += abs (va_arg (ap, int));
 		}
 	      else
-	      total_width += strtoul (p, &p, 10);
+	      total_width += strtoul (p, (char **)&p, 10);
 	    }
 	  while (strchr ("hlL", *p))
 	    ++p;
