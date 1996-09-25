fe0d2a604fc70a0c53942b016255b047abd60573
Jim Blandy <jimb@floss.cyclic.com>
1996-09-25 08:18:17 +0000

    * update.c (update): Don't neglect to pass the -kmumble options to the server. * sanity.sh (binfiles-sticky): New tests for above.
    * sanity.sh: When testing rsh, use the program specified by
    the CVS_RSH environment variable, if it's set.  Move test to top
    of file, so it runs before all other tests (it's really a
    meta-test).
    
    CVS-ID: src/sanity.sh 1.132

--- src/sanity.sh
+++ src/sanity.sh
@@ -30,6 +30,12 @@ echo 'This test should produce no other output than this line, and a final "OK".
 if test x"$1" = x"-r"; then
 	shift
 	remote=yes
+	# If we're going to do remote testing, make sure 'rsh' works first.
+        host="`hostname`"
+	if test "x`${CVS_RSH-rsh} $host 'echo hi'`" != "xhi"; then
+	    echo "ERROR: cannot test remote CVS, because \`rsh $host' fails." >&2
+	    exit 1
+	fi
 else
 	remote=no
 fi
@@ -466,13 +472,7 @@ CVSROOT=${CVSROOT_DIRNAME} ; export CVSROOT
 if test "x$remote" = xyes; then
 	# Use rsh so we can test it without having to muck with inetd
 	# or anything like that.  Also needed to get CVS_SERVER to
-	# work.  Make sure 'rsh' works altogether.
-        host="`hostname`"
-	if test "x`rsh $host 'echo hi'`" != "xhi"; then
-	    echo "ERROR: cannot test remote CVS, because \`rsh $host' fails." >&2
-	    exit 1
-	fi
-
+	# work.
 	CVSROOT=:ext:`hostname`:${CVSROOT_DIRNAME} ; export CVSROOT
 	CVS_SERVER=${testcvs}; export CVS_SERVER
 fi
@@ -4041,6 +4041,7 @@ done'
 	    dotest binfiles-12 "${testcvs} -q update -A" '[UP] binfile'
 	    dotest binfiles-13 "${testcvs} -q update -A" ''
 	  fi
+
 	  cd ../..
 	  rm -rf 1
 
@@ -4107,6 +4108,30 @@ File: binfile          	Status: Up-to-date
    Sticky Tag:		(none)
    Sticky Date:		(none)
    Sticky Options:	-kv'
+
+	  # Do sticky options work when used with 'cvs update'?
+	  echo "Not a binary file." > nibfile
+	  dotest binfiles-sticky1 "${testcvs} -q add nibfile" \
+	    'cvs [a-z]*: use '\''cvs commit'\'' to add this file permanently'
+	  dotest binfiles-sticky2 "${testcvs} -q ci -m add-it nibfile" \
+	    'RCS file: /tmp/cvs-sanity/cvsroot/first-dir/nibfile,v
+done
+Checking in nibfile;
+/tmp/cvs-sanity/cvsroot/first-dir/nibfile,v  <--  nibfile
+initial revision: 1.1
+done'
+	  dotest binfiles-sticky3 "${testcvs} -q update -kb nibfile" \
+	    '[UP] nibfile'
+	  dotest binfiles-sticky4 "${testcvs} -q status nibfile" \
+'===================================================================
+File: nibfile          	Status: Up-to-date
+
+   Working revision:	1\.1.*
+   Repository revision:	1\.1	/tmp/cvs-sanity/cvsroot/first-dir/nibfile,v
+   Sticky Tag:		(none)
+   Sticky Date:		(none)
+   Sticky Options:	-kb'
+	  # Eventually we should test that -A removes the -kb here...
 
 	  cd ../..
 	  rm -rf ${CVSROOT_DIRNAME}/first-dir
