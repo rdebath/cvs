0a72822c9781131225be5c798adf769415f5776d
Jim Blandy <jimb@floss.cyclic.com>
1996-10-02 07:51:32 +0000

    CVS-ID: src/version.c 1.48

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.8.86";
+char *version_string = "\nConcurrent Versions System (CVS) 1.8.87";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
