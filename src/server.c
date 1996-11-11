c2ab57152194c2d4cee349e6e552e0a552f0410c
Jim Kingdon <kingdon@redhat.com>
1996-11-11 03:22:04 +0000

    * server.c: Include a "copyright" notice.
    	* server.c: If HAVE_WINSOCK_H, include winsock.h.
    
    CVS-ID: src/ChangeLog 1.889
    CVS-ID: src/server.c 1.127

--- src/server.c
+++ src/server.c
@@ -1,3 +1,13 @@
+/* This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.  */
+
 #include <assert.h>
 #include "cvs.h"
 #include "watch.h"
@@ -7,6 +17,10 @@
 #include "buffer.h"
 
 #ifdef SERVER_SUPPORT
+
+#ifdef HAVE_WINSOCK_H
+#include <winsock.h>
+#endif
 
 #if defined (AUTH_SERVER_SUPPORT) || defined (HAVE_KERBEROS)
 #include <sys/socket.h>
