4649cae2c7ffeefbac5299451fe98341d9454794
Jim Kingdon <kingdon@redhat.com>
1996-11-11 03:35:46 +0000

    * server.c: Use all the right cruft which goes along with 	including sys/time.h.
    CVS-ID: src/ChangeLog 1.890
    CVS-ID: src/server.c 1.128

--- src/server.c
+++ src/server.c
@@ -44,7 +44,17 @@ static Key_schedule sched;
 #ifdef HAVE_SYS_BSDTYPES_H
 #include <sys/bsdtypes.h>
 #endif
-#include <sys/time.h>
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 
 #if HAVE_SYS_SELECT_H
 #include <sys/select.h>
