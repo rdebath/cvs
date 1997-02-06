a596343791a89cc8e067ae07acdc64c383121746
Jim Kingdon <kingdon@redhat.com>
1997-02-06 16:49:21 +0000

    * patch.c (patch): Give a fatal error for -V option (see comment 	for rationale).
    CVS-ID: src/ChangeLog 1.954
    CVS-ID: src/patch.c 1.51

--- src/patch.c
+++ src/patch.c
@@ -124,12 +124,31 @@ patch (argc, argv)
 		options = RCS_check_kflag (optarg);
 		break;
 	    case 'V':
+		/* This option is pretty seriously broken:
+		   1.  It is not clear what it does (does it change keyword
+		   expansion behavior?  If so, how?  Or does it have
+		   something to do with what version of RCS we are using?
+		   Or the format we write RCS files in?).
+		   2.  Because both it and -k use the options variable,
+		   specifying both -V and -k doesn't work.
+		   3.  At least as of CVS 1.9, it doesn't work (failed
+		   assertion in RCS_checkout where it asserts that options
+		   starts with -k).  Few people seem to be complaining.
+		   In the future (perhaps the near future), I have in mind
+		   removing it entirely, and updating NEWS and cvs.texinfo,
+		   but in case it is a good idea to give people more time
+		   to complain if they would miss it, I'll just add this
+		   quick and dirty error message for now.  */
+		error (1, 0,
+		       "the -V option is obsolete and should not be used");
+#if 0
 		if (atoi (optarg) <= 0)
 		    error (1, 0, "must specify a version number to -V");
 		if (options)
 		    free (options);
 		options = xmalloc (strlen (optarg) + 1 + 2);	/* for the -V */
 		(void) sprintf (options, "-V%s", optarg);
+#endif
 		break;
 	    case 'u':
 		unidiff = 1;		/* Unidiff */
