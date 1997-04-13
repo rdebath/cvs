bb15ca29f871bd30208e92843abdb0ab131da25d
Jim Kingdon <kingdon@redhat.com>
1997-04-13 22:09:16 +0000

    * checkout.c (build_one_dir): New function. 	(struct dir_to_build): New structure. 	(build_dirs_and_chir): Rewritten to accept a linked list of struct 	dir_to_build rather than the silly string processing we had been 	doing before. 	(checkout_proc): Rewrite code that calls build_dirs_and_chdir 	accordingly. 	* sanity.sh: Enable tests modules3-10 and modules3-11 for local CVS; 	tests for above fix.
    CVS-ID: src/ChangeLog 1.1048
    CVS-ID: src/checkout.c 1.55
    CVS-ID: src/sanity.sh 1.214

--- src/sanity.sh
+++ src/sanity.sh
@@ -4118,11 +4118,8 @@ done'
 	  # Try the same thing, but with the directories nested even
 	  # deeper (deeply enough so they are nested more deeply than
 	  # the number of directories from / to ${TESTDIR}).
-	  if false; then
-	  # This one fails with a SIGSEGV.  Based on looking at the
-	  # CVS/Repository files that modules3-8 creates locally, it would
-	  # seem that the same underlying bug is responsible for this and
-	  # the remote failures in modules3-8.
+	  if test "x$remote" = xno; then
+	  # I assume this will be like modules3-8 for remote
 	  mkdir 1
 	  cd 1
 	  dotest modules3-10 "${testcvs} -q co nestdeeper" \
@@ -4130,7 +4127,7 @@ done'
 	  dotest modules3-11 "test -f src/sub1/sub2/sub3/dir/file1" ''
 	  cd ..
 	  rm -r 1
-	  fi # matches "if false".
+	  fi # end of tests bypassed for remote
 
 	  # This one is almost too twisted for words.  The pathname output
 	  # in the message from "co" doesn't include the "path/in/modules",
