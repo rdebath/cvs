506cc01ea89cfd53df2895db45427ad614b384a8
Jim Kingdon <kingdon@redhat.com>
1997-05-08 15:41:22 +0000

    * cvsbug.sh: Remove $Id; we decided to get rid of these some time 	ago.
    CVS-ID: src/ChangeLog 1.1091
    CVS-ID: src/cvsbug.sh 1.13

--- src/cvsbug.sh
+++ src/cvsbug.sh
@@ -7,8 +7,6 @@
 # This file is part of GNU GNATS.
 # Modified by Berliner for CVS.
 #
-#ident	"@(#)cvs/src:$Name$:$Id$"
-#
 # GNU GNATS is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)
