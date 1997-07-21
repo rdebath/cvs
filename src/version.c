e08b112d91f700333c1ae02e84af607f525323c0
Jim Kingdon <kingdon@redhat.com>
1997-07-21 22:08:45 +0000

    * version.c: Change version number to 1.9.13.
    CVS-ID: src/ChangeLog 1.1160
    CVS-ID: src/version.c 1.62

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.9.12";
+char *version_string = "\nConcurrent Versions System (CVS) 1.9.13";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
