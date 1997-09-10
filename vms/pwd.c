1f182704c95b313d3b1af58a0f73bc0602111b96
Jim Kingdon <kingdon@redhat.com>
1997-09-10 00:46:03 +0000

    * pwd.c (getpwuid): Declare argument as unsigned int not uid_t.
    CVS-ID: vms/ChangeLog 1.37
    CVS-ID: vms/pwd.c 1.3

--- vms/pwd.c
+++ vms/pwd.c
@@ -8,7 +8,7 @@ static struct passwd pw;
    the code is really trying to figure out who owns a directory.  Nothing
    which has anything to do with getpwuid or anything of the sort can help
    us on VMS (getuid returns only the group part of the UIC).  */
-struct passwd *getpwuid(uid_t uid)
+struct passwd *getpwuid(unsigned int uid)
 {
     return NULL;
 }
