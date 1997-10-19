4cfe5f7b49f539493dbfe1f005fecf8b3a8922c0
Jim Kingdon <kingdon@redhat.com>
1997-10-19 03:32:16 +0000

    * Version 1.9.18.
    CVS-ID: src/ChangeLog 1.1267
    CVS-ID: src/version.c 1.67

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.9.17";
+char *version_string = "\nConcurrent Versions System (CVS) 1.9.18";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
