59e6561b8f7361a61f8d5b5d0f2da9d754fcc7e5
Jim Kingdon <kingdon@redhat.com>
1997-11-18 13:43:33 +0000

    * version.c: Change version number to 1.9.21.
    CVS-ID: src/ChangeLog 1.1331
    CVS-ID: src/version.c 1.70

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.9.20";
+char *version_string = "\nConcurrent Versions System (CVS) 1.9.21";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
