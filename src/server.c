b54d725046d642328052a4c1380fc50b993edbd2
Samuel Tardieu <sam@inf.enst.fr>
1998-03-17 14:24:13 +0000

    * server.c (server_register): protect dereferencing timestamp in the trace message when it is null, to avoid a segmentation fault.
    CVS-ID: src/ChangeLog 1.1479
    CVS-ID: src/server.c 1.185

--- src/server.c
+++ src/server.c
@@ -2840,8 +2840,9 @@ server_register (name, version, timestamp, options, tag, date, conflict)
 	(void) fprintf (stderr,
 			"%c-> server_register(%s, %s, %s, %s, %s, %s, %s)\n",
 			(server_active) ? 'S' : ' ', /* silly */
-			name, version, timestamp, options, tag ? tag : "",
-			date ? date : "", conflict ? conflict : "");
+			name, version, timestamp ? timestamp : "", options,
+			tag ? tag : "", date ? date : "",
+			conflict ? conflict : "");
     }
 
     if (entries_line != NULL)
