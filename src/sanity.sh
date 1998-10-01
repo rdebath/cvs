1a2256f97fc30da4c20a56dd66d6311694721e9f
Jim Kingdon <kingdon@redhat.com>
1998-10-01 13:53:34 +0000

    * sanity.sh (devcom-180): Allow one digit day.
    CVS-ID: src/ChangeLog 1.1629
    CVS-ID: src/sanity.sh 1.452

--- src/sanity.sh
+++ src/sanity.sh
@@ -8986,7 +8986,7 @@ done"
 	  # strings should vary based on locale does not strike me as
 	  # self-evident.
 	  dotest devcom-180 "${testcvs} editors" \
-"abb	${username}	[SMTWF][uoehra][neduit] [JFAMSOND][aepuco][nbrylgptvc] [0-9]* [0-9:]* [0-9][0-9][0-9][0-9] GMT	[-a-zA-Z_.0-9]*	${TESTDIR}/2/first-dir"
+"abb	${username}	[SMTWF][uoehra][neduit] [JFAMSOND][aepuco][nbrylgptvc] [0-9 ][0-9] [0-9:]* [0-9][0-9][0-9][0-9] GMT	[-a-zA-Z_.0-9]*	${TESTDIR}/2/first-dir"
 
 	  echo aaaa >>abb
 	  if ${testcvs} ci -m modify abb >>${LOGFILE} 2>&1; then
