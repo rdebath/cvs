53d6cf771e0dcbf8dbf4410d253d708126bf630b
Jim Kingdon <kingdon@redhat.com>
1999-01-12 20:36:09 +0000

    * Makefile.in, analyze.c, cmpbuf.c, cmpbuf.h, context.c, diff.c, 	diff.h, diff3.c, diffrun.h, dir.c, ed.c, io.c, normal.c, system.h, 	util.c: Remove paragraph containing the old snail mail address of 	the Free Software Foundation.
    CVS-ID: diff/ChangeLog 1.26
    CVS-ID: diff/Makefile.in 1.12
    CVS-ID: diff/analyze.c 1.4
    CVS-ID: diff/cmpbuf.c 1.2
    CVS-ID: diff/cmpbuf.h 1.2
    CVS-ID: diff/context.c 1.3
    CVS-ID: diff/diff.c 1.8
    CVS-ID: diff/diff.h 1.4
    CVS-ID: diff/diff3.c 1.7
    CVS-ID: diff/diffrun.h 1.4
    CVS-ID: diff/dir.c 1.3
    CVS-ID: diff/ed.c 1.3
    CVS-ID: diff/io.c 1.2
    CVS-ID: diff/normal.c 1.3
    CVS-ID: diff/system.h 1.4
    CVS-ID: diff/util.c 1.10

--- diff/system.h
+++ diff/system.h
@@ -13,9 +13,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GNU DIFF; see the file COPYING.  If not, write to
-the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+*/
 
 /* We must define `volatile' and `const' first (the latter inside config.h),
    so that they're used consistently in all system includes.  */
