78889da2461dc581084fe40ec5c0f3705c22db1d
Jim Kingdon <kingdon@redhat.com>
1999-02-22 18:02:29 +0000

    * version.c: Version 1.10.5.
    CVS-ID: src/ChangeLog 1.1713
    CVS-ID: src/version.c 1.93

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.10.4.1";
+char *version_string = "\nConcurrent Versions System (CVS) 1.10.5";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
