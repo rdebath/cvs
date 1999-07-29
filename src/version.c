30fd299be50e2420733a338c365e8547b020321b
Eric Sink <eric@sourcegear.com>
1999-07-29 18:43:27 +0000

    changed for post 1.10.7
    CVS-ID: src/version.c 1.98

--- src/version.c
+++ src/version.c
@@ -12,7 +12,7 @@
 
 #include "cvs.h"
 
-char *version_string = "\nConcurrent Versions System (CVS) 1.10.7";
+char *version_string = "\nConcurrent Versions System (CVS) 1.10.7.1";
 
 #ifdef CLIENT_SUPPORT
 #ifdef SERVER_SUPPORT
