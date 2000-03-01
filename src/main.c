dc9e0b8099cdf09b03f3b0669365e5acc06c1a65
Larry Jones <lawrence.jones@siemens.com>
2000-03-01 16:35:18 +0000

    * main.c (main): Update year in copyright message.
    CVS-ID: src/ChangeLog 1.1872
    CVS-ID: src/main.c 1.144

--- src/main.c
+++ src/main.c
@@ -558,7 +558,7 @@ main (argc, argv)
 		(void) fputs (config_string, stdout);
 		(void) fputs ("\n", stdout);
 		(void) fputs ("\
-Copyright (c) 1989-1998 Brian Berliner, david d `zoo' zuhn, \n\
+Copyright (c) 1989-2000 Brian Berliner, david d `zoo' zuhn, \n\
                         Jeff Polk, and other authors\n", stdout);
 		(void) fputs ("\n", stdout);
 		(void) fputs ("CVS may be copied only under the terms of the GNU General Public License,\n", stdout);
