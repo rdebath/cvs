669328c0e6bec04351c7650f48ce96941189b9ec
Mark D. Baushke <mdb@gnu.org>
2003-03-04 23:42:34 +0000

    Localize the setting of CVS_LOCAL_BRANCH_NUM environment variable.
    CVS-ID: src/ChangeLog 1.2372
    CVS-ID: src/sanity.sh 1.765

--- src/sanity.sh
+++ src/sanity.sh
@@ -5711,8 +5711,8 @@ initial revision: 1\.1
 done"
 
 	  # Tag the file using a CVS_LOCAL_BRANCH_NUM of 1000
-	  CVS_LOCAL_BRANCH_NUM=1000 \
-	  dotest branches3-4 "${testcvs} -q tag -b tag1" 'T file1'
+	  (CVS_LOCAL_BRANCH_NUM=1000 \
+	  dotest branches3-4 "${testcvs} -q tag -b tag1" 'T file1')
 	  dotest branches3-5 "${testcvs} -q log file1" \
 "
 RCS file: ${CVSROOT_DIRNAME}/first-dir/file1,v
