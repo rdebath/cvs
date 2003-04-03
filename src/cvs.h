6d89436eb449f074bcf5dd4b1d76efc434340fa2
Derek R. Price <derek@ximbiot.com>
2003-04-03 15:44:49 +0000

    * cvs.h: Avoid __pure__ for GCC versions < 2.96 & __malloc__ for GCC versions < 3.0.
    CVS-ID: src/ChangeLog 1.2413
    CVS-ID: src/cvs.h 1.250

--- src/cvs.h
+++ src/cvs.h
@@ -38,16 +38,21 @@
 /* This feature is available in gcc versions 2.5 and later.  */
 # if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
 #  define __attribute__(Spec) /* empty */
+# else
+#   if __GNUC__ == 2 && __GNUC_MINOR__ < 96
+#    define __pure__	/* empty */
+#   endif
+#   if __GNUC__ < 3
+#    define __malloc__	/* empty */
+#   endif
 # endif
 /* The __-protected variants of `format' and `printf' attributes
    are accepted by gcc versions 2.6.4 (effectively 2.7) and later.  */
 # if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
 #  define __const__	const
 #  define __format__	format
-#  define __malloc__	malloc
 #  define __noreturn__	noreturn
 #  define __printf__	printf
-#  define __pure__	pure
 # endif
 #endif
 
