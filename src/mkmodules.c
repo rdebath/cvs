89c57b6032789e272ed5f89b69fdbac66fb9ebc6
Derek R. Price <derek@ximbiot.com>
2003-05-20 19:15:46 +0000

    * mkmodules.c (config_contents): Add missing newline. (Patch from Kenneth Lorber <keni@his.com>.)
    CVS-ID: src/ChangeLog 1.2443
    CVS-ID: src/mkmodules.c 1.68

--- src/mkmodules.c
+++ src/mkmodules.c
@@ -301,7 +301,7 @@ static const char *const config_contents[] = {
     "#LogHistory=TOFEWGCMAR\n",
     "\n",
     "# Set `RereadLogAfterVerify' to `always' (the default) to allow the verifymsg\n",
-    "# script to change the log message.  Set it to `stat' to force CVS to verify",
+    "# script to change the log message.  Set it to `stat' to force CVS to verify\n",
     "# that the file has changed before reading it (this can take up to an extra\n",
     "# second per directory being committed, so it is not recommended for large\n",
     "# repositories.  Set it to `never' (the previous CVS behavior) to prevent\n",
