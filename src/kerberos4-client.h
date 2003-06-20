5bee532e5b87049c1d31b9e3b71007c7aa64da0a
Derek R. Price <derek@ximbiot.com>
2003-06-20 22:27:15 +0000

    * kerberos-client.c, kerberos-client.h, client.c: Split out Kerberos 4 code to separate files.
    * Makefile.am: Mention new files.
    (Patch from Alexey Mahotkin <alexm@hsys.msk.ru>.)
    
    * Makefile.in: Regenerated.
    
    CVS-ID: src/ChangeLog 1.2476
    CVS-ID: src/Makefile.am 1.29
    CVS-ID: src/Makefile.in 1.99
    CVS-ID: src/kerberos4-client.c 1.1
    CVS-ID: src/kerberos4-client.h 1.1

---
+++ src/kerberos4-client.h
@@ -0,0 +1,25 @@
+/* CVS Kerberos4 client stuff.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.  */
+
+
+#ifndef KERBEROS4_CLIENT_H__
+#define KERBEROS4_CLIENT_H__
+
+extern void start_tcp_server PROTO((cvsroot_t *root,
+				    struct buffer **to_server_p,
+				    struct buffer **from_server_p));
+
+extern void initialize_kerberos4_encryption_buffers PROTO((struct buffer **to_server_p,
+							   struct buffer **from_server_p));
+
+#endif
+
