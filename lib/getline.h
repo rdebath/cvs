d739a68c666f9f46e3c66005d5dcfe3f5c04cfda
Derek R. Price <derek@ximbiot.com>
2003-07-17 00:52:01 +0000

    * getline.h: Include config.h so to use the macros it defines. (Thanks to Steve McIntyre <stevem@cvshome.org> for the report.) Issue number:  130
    CVS-ID: lib/ChangeLog 1.200
    CVS-ID: lib/getline.h 1.8

--- lib/getline.h
+++ lib/getline.h
@@ -20,6 +20,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #ifndef GETLINE_H_
 # define GETLINE_H_ 1
 
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 # include <stddef.h>
 # include <stdio.h>
 
