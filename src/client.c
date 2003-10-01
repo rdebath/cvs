cae8f0b58dd91d5b8d78d2c6fce99f8d5b0aeb5b
Derek R. Price <derek@ximbiot.com>
2003-10-01 22:12:35 +0000

    * client.c (connect_to_forked_server): Use TRACE macro rather than the old style.
    CVS-ID: src/ChangeLog 1.2548
    CVS-ID: src/client.c 1.349

--- src/client.c
+++ src/client.c
@@ -3371,10 +3371,7 @@ connect_to_forked_server( struct buffer **to_server_p,
     command[1] = "server";
     command[2] = NULL;
 
-    if (trace)
-    {
-	fprintf (stderr, " -> Forking server: %s %s\n", command[0], command[1]);
-    }
+    TRACE (TRACE_FUNCTION, "Forking server: %s %s\n", command[0], command[1]);
 
     child_pid = piped_child (command, &tofd, &fromfd);
     if (child_pid < 0)
