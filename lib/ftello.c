47b1392cb3414c5bb0b8522ec99071c614e43fd8
Mark D. Baushke <mdb@gnu.org>
2003-12-10 20:43:36 +0000

    * configure.in(AC_FUNC_FSEEKO): Add. Include handling for fseeko and ftello with AC_LIBOBJ. (AC_REPLACE_FUNCS): Remove fseeko and ftello.
    * lib/ftello.c: Rework for AC_FUNC_FSEEKO.
    * lib/system.h: Rework fseeko and ftello prototypes.
    
    CVS-ID: lib/ChangeLog 1.243
    CVS-ID: lib/ftello.c 1.2
    CVS-ID: lib/system.h 1.60
    CVS-ID: windows-NT/config.h 1.59
    CVS-ID: ChangeLog 1.864
    CVS-ID: config.h.in 1.109
    CVS-ID: configure 1.258
    CVS-ID: configure.in 1.249

--- lib/ftello.c
+++ lib/ftello.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+#if !defined(HAVE_FSEEKO) && !defined(ftello)
 off_t
 ftello (FILE *stream)
 {
@@ -26,3 +27,4 @@ ftello (FILE *stream)
 
     return (off_t) pos;
 }
+#endif /* !defined(HAVE_FSEEKO) && !defined(ftello) */
--- lib/system.h
+++ lib/system.h
@@ -465,9 +465,17 @@ extern int errno;
 #define CVS_FNMATCH fnmatch
 #endif
 
-#ifndef HAVE_FTELLO
+#ifndef HAVE_FSEEKO
+
+#ifndef ftello
 off_t ftello (FILE *);
 #endif
+
+#ifndef fseeko
+int fseeko (FILE *, off_t, int);
+#endif
+
+#endif /* HAVE_FSEEKO */
 
 #if defined (__CYGWIN32__) || defined (WIN32)
 /*
