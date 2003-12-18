22db5f1561ee2d2720fd99390f24674890b9dfc2
Derek R. Price <derek@ximbiot.com>
2003-12-18 17:45:36 +0000

    Merge changes from 1.11.x.
    CVS-ID: src/ChangeLog 1.2646
    CVS-ID: src/sanity.sh 1.862
    CVS-ID: src/server.c 1.332
    CVS-ID: ChangeLog 1.868
    CVS-ID: NEWS 1.173

--- src/server.c
+++ src/server.c
@@ -5114,6 +5114,12 @@ error 0 %s: no such system user\n", username);
 	exit (EXIT_FAILURE);
     }
 
+    if (pw->pw_uid == 0)
+    {
+        printf("error 0: root not allowed\n");
+        exit (EXIT_FAILURE);
+    }
+
 #if HAVE_INITGROUPS
     if (initgroups (pw->pw_name, pw->pw_gid) < 0
 #  ifdef EPERM
