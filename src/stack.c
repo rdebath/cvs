ea6b51ff29442c1e31c4547ba1a914c9bd6f1c4c
Derek R. Price <derek@ximbiot.com>
2004-02-04 21:13:56 +0000

    * modules.c (do_modules): Move content to and make this function a wrapper for... (_do_modules): ...this new function which can watch for infinite loops in alias modules. * stack.c (_push, _pop, _unshift, _shift, push_string, pop_string, unshift_string, shift_string): New functions. * stack.h (push_string, pop_string, unshift_string, shift_string: New prototypes. * sanity.sh (modules): Add check for nested alias loops.
    CVS-ID: src/stack.c 1.1.2.2
    CVS-ID: src/stack.h 1.1.2.2

--- src/stack.c
+++ src/stack.c
@@ -14,29 +14,102 @@
 
 
 
-void
-push (stack, elem)
+static void
+_push (stack, elem, isstring)
     List *stack;
     void *elem;
+    int isstring;
 {
     Node *p = getnode();
-    p->data = elem;
+
+    if (isstring)
+	p->key = elem;
+    else
+	p->data = elem;
+
     addnode(stack, p);
 }
 
 
 
-void *pop (stack)
+void
+push (stack, elem)
     List *stack;
+    void *elem;
+{
+    _push (stack, elem, 0);
+}
+
+
+
+void
+push_string (stack, elem)
+    List *stack;
+    char *elem;
+{
+    _push (stack, elem, 1);
+}
+
+
+
+static void *
+_pop (stack, isstring)
+    List *stack;
+    int isstring;
 {
     void *elem;
 
     if (isempty (stack)) return NULL;
 
-    elem = stack->list->prev->data;
-    stack->list->prev->data = NULL;
+    if (isstring)
+    {
+	elem = stack->list->prev->key;
+	stack->list->prev->key = NULL;
+    }
+    else
+    {
+	elem = stack->list->prev->data;
+	stack->list->prev->data = NULL;
+    }
+
     delnode (stack->list->prev);
     return elem;
+}
+
+
+
+void *
+pop (stack)
+    List *stack;
+{
+    return _pop (stack, 0);
+}
+
+
+
+char *
+pop_string (stack)
+    List *stack;
+{
+    return _pop (stack, 1);
+}
+
+
+
+static void
+_unshift (stack, elem, isstring)
+    List *stack;
+    void *elem;
+    int isstring;
+{
+    Node *p = getnode();
+
+    if (isstring)
+	p->key = elem;
+    else
+	p->data = elem;
+
+    addnode_at_front(stack, p);
 }
 
 
@@ -46,25 +119,63 @@ unshift (stack, elem)
     List *stack;
     void *elem;
 {
-    Node *p = getnode();
-    p->data = elem;
-    addnode_at_front(stack, p);
+    _unshift (stack, elem, 0);
 }
 
 
 
-void *shift (stack)
+void
+unshift_string (stack, elem)
     List *stack;
+    char *elem;
+{
+    _unshift (stack, elem, 1);
+}
+
+
+
+static void *
+_shift (stack, isstring)
+    List *stack;
+    int isstring;
 {
     void *elem;
 
     if (isempty (stack)) return NULL;
 
-    elem = stack->list->next->data;
-    stack->list->next->data = NULL;
+    if (isstring)
+    {
+	elem = stack->list->next->key;
+	stack->list->next->key = NULL;
+    }
+    else
+    {
+	elem = stack->list->next->data;
+	stack->list->next->data = NULL;
+    }
     delnode (stack->list->next);
     return elem;
 }
+
+
+
+void *
+shift (stack)
+    List *stack;
+{
+    _shift (stack, 0);
+}
+
+
+
+char *
+shift_string (stack)
+    List *stack;
+{
+    _shift (stack, 1);
+}
+
+
 
 int
 isempty (stack)
--- src/stack.h
+++ src/stack.h
@@ -11,4 +11,8 @@ void push PROTO((List *_stack, void *_elem));
 void *pop PROTO((List *_stack));
 void unshift PROTO((List *_stack, void *_elem));
 void *shift PROTO((List *_stack));
+void push_string PROTO((List *_stack, char *_elem));
+char *pop_string PROTO((List *_stack));
+void unshift_string PROTO((List *_stack, char *_elem));
+char *shift_string PROTO((List *_stack));
 int isempty PROTO((List *_stack));
