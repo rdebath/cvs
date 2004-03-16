4da128b9211bdff8aff389ade88273fb1dbe7d5d
Mark D. Baushke <mdb@gnu.org>
2004-03-16 07:38:08 +0000

    Merge changes from 1.11.x.
    CVS-ID: src/ChangeLog 1.2740
    CVS-ID: src/release.c 1.56

--- src/release.c
+++ src/release.c
@@ -115,6 +115,9 @@ release (int argc, char **argv)
     /* Construct the update command.  Be sure to add authentication and
        encryption if we are using them currently, else our child process may
        not be able to communicate with the server.  */
+    update_cmd = xmalloc (strlen (program_path)
+                        + strlen (current_parsed_root->original)
+                        + 1 + 3 + 3 + 16 + 1);
     sprintf (update_cmd, "%s %s%s-n -q -d %s update",
              program_path,
              cvsauthenticate ? "-a " : "",
