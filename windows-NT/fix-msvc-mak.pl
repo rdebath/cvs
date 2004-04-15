ab5773909e5d8b68d5be62acfe12fd21549c128f
Derek R. Price <derek@ximbiot.com>
2004-04-15 14:21:06 +0000

    * fix-msvc-mak.pl: Set default build configuration to Debug.
    CVS-ID: windows-NT/ChangeLog 1.208
    CVS-ID: windows-NT/fix-msvc-mak.pl 1.2

--- windows-NT/fix-msvc-mak.pl
+++ windows-NT/fix-msvc-mak.pl
@@ -1,5 +1,7 @@
 use strict;
 
+
+
 sub save_edit {
 	my ($found, $file_name, $temp_name) = @_;
 
@@ -12,10 +14,11 @@ sub save_edit {
 	}
 }
 
+
+
 sub fix_cvsnt_dep {
-	my $file_base = "cvsnt";
-	my $file_name = $file_base . ".dep";
-	my $temp_name = $file_base . ".tmp";
+	my $file_name = "cvsnt.dep";
+	my $temp_name = $file_name . ".tmp";
 
 	open FINP, "< " . $file_name or die "open error: ", $file_name;
 	open FOUT, "> " . $temp_name or die "open error: ", $temp_name;
@@ -35,10 +38,11 @@ sub fix_cvsnt_dep {
 	save_edit $found, $file_name, $temp_name;
 }
 
+
+
 sub fix_libdiff_mak {
-	my $file_base = "diff/libdiff";
-	my $file_name = $file_base . ".mak";
-	my $temp_name = $file_base . ".tmp";
+	my $file_name = "diff/libdiff.mak";
+	my $temp_name = $file_name . ".tmp";
 
 	open FINP, "< " . $file_name or die "open error: ", $file_name;
 	open FOUT, "> " . $temp_name or die "open error: ", $temp_name;
@@ -58,5 +62,41 @@ sub fix_libdiff_mak {
 	save_edit $found, $file_name, $temp_name;
 }
 
+
+
+sub set_default_configuration {
+	my ($file_name) = @_;
+	my $temp_name = $file_name . ".tmp";
+
+	open FINP, "< " . $file_name or die "open error: ", $file_name;
+	open FOUT, "> " . $temp_name or die "open error: ", $temp_name;
+
+	my $found = 0;
+	while (<FINP>) {
+		if (s/^(CFG=\w+ - Win32 )Release$/$1Debug/)
+		{
+			$found += 1;
+		}
+		elsif (s/(Defaulting to \w+ - Win32 )Release\.$/$1Debug/)
+		{
+			$found += 1;
+		}
+		print FOUT $_;
+	}
+
+	close FOUT;
+	close FINP;
+
+	save_edit $found, $file_name, $temp_name;
+}
+
+
+
 fix_cvsnt_dep;
 fix_libdiff_mak;
+
+foreach my $file ("cvsnt.mak", "lib/LIB.mak", "diff/libdiff.mak",
+                  "zlib/zlib.mak")
+{
+	set_default_configuration $file;
+}
