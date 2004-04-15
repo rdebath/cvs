14bae8af602b67d94b7d2d46ef6970985029f6fa
Derek R. Price <derek@ximbiot.com>
2004-04-15 17:03:42 +0000

    * fix-msvc-mak.pl: Remove default build configuration hooks.
    CVS-ID: windows-NT/ChangeLog 1.209
    CVS-ID: windows-NT/fix-msvc-mak.pl 1.3

--- windows-NT/fix-msvc-mak.pl
+++ windows-NT/fix-msvc-mak.pl
@@ -64,39 +64,6 @@ sub fix_libdiff_mak {
 
 
 
-sub set_default_configuration {
-	my ($file_name) = @_;
-	my $temp_name = $file_name . ".tmp";
-
-	open FINP, "< " . $file_name or die "open error: ", $file_name;
-	open FOUT, "> " . $temp_name or die "open error: ", $temp_name;
-
-	my $found = 0;
-	while (<FINP>) {
-		if (s/^(CFG=\w+ - Win32 )Release$/$1Debug/)
-		{
-			$found += 1;
-		}
-		elsif (s/(Defaulting to \w+ - Win32 )Release\.$/$1Debug/)
-		{
-			$found += 1;
-		}
-		print FOUT $_;
-	}
-
-	close FOUT;
-	close FINP;
-
-	save_edit $found, $file_name, $temp_name;
-}
-
-
 
 fix_cvsnt_dep;
 fix_libdiff_mak;
-
-foreach my $file ("cvsnt.mak", "lib/LIB.mak", "diff/libdiff.mak",
-                  "zlib/zlib.mak")
-{
-	set_default_configuration $file;
-}
