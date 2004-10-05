5a7fded1f6b04990ab96c40a1e9339c2d71252e3
Derek R. Price <derek@ximbiot.com>
2004-10-05 17:59:47 +0000

    * Makefile.am (libcvs_a_SOURCES): Move regex.h to its own section. * regex.c: Import most recent version from GNULIB.
    CVS-ID: lib/ChangeLog 1.315
    CVS-ID: lib/Makefile.am 1.54
    CVS-ID: lib/Makefile.in 1.126
    CVS-ID: lib/regex.c 1.16

--- lib/regex.c
+++ lib/regex.c
@@ -4,7 +4,7 @@
    internationalization features.)
 
    Copyright (C) 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
-   2002, 2003 Free Software Foundation, Inc.
+   2002, 2003, 2004 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -275,13 +275,7 @@ init_syntax_once (void)
 #  ifndef alloca
 
 /* Make alloca work the best possible way.  */
-#   ifdef __GNUC__
-#    define alloca __builtin_alloca
-#   else /* not __GNUC__ */
-#    if HAVE_ALLOCA_H
-#     include <alloca.h>
-#    endif /* HAVE_ALLOCA_H */
-#   endif /* not __GNUC__ */
+#   include <alloca.h>
 
 #  endif /* not alloca */
 
@@ -589,7 +583,10 @@ typedef enum
 #  undef INSIDE_RECURSION
 # endif
 #endif
-#include "unlocked-io.h"
+
+#if USE_UNLOCKED_IO
+# include "unlocked-io.h"
+#endif
 
 #ifdef INSIDE_RECURSION
 /* Common operations on the compiled pattern.  */
@@ -4973,7 +4970,7 @@ weak_alias (__re_search_2, re_search_2)
 #ifdef INSIDE_RECURSION
 
 #ifdef MATCH_MAY_ALLOCATE
-# define FREE_VAR(var) if (var) (void)REGEX_FREE ((void *)var); var = NULL
+# define FREE_VAR(var) if (var) REGEX_FREE (var); var = NULL
 #else
 # define FREE_VAR(var) if (var) free (var); var = NULL
 #endif
