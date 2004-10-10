52ae092ccacf39c9eb81a108ff49c02dea45ab1b
Derek R. Price <derek@ximbiot.com>
2004-10-10 03:54:54 +0000

    * Makefile.am (libcvs_a_SOURCES): Add stat-macros.h. * stat.c: Update from GNULIB. * stat-macros.c: New file from GNULIB.
    CVS-ID: lib/ChangeLog 1.343
    CVS-ID: lib/Makefile.am 1.69
    CVS-ID: lib/Makefile.gnulib 1.23
    CVS-ID: lib/Makefile.in 1.142
    CVS-ID: lib/stat-macros.h 1.1
    CVS-ID: lib/stat.c 1.5

--- lib/stat.c
+++ lib/stat.c
@@ -3,7 +3,7 @@
    has this bug.  Also work around a deficiency in Solaris systems (up to at
    least Solaris 9) regarding the semantics of `lstat ("symlink/", sbuf).'
 
-   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003 Free
+   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004 Free
    Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
@@ -27,36 +27,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-#ifndef errno
-extern int errno;
-#endif
 #if defined LSTAT && ! LSTAT_FOLLOWS_SLASHED_SYMLINK
+# include <stdlib.h>
 # include <string.h>
 
-# if HAVE_STDLIB_H
-#  include <stdlib.h>
-# endif
-
-# ifdef STAT_MACROS_BROKEN
-#  undef S_ISLNK
-# endif
-
-# ifndef S_ISLNK
-#  ifdef S_IFLNK
-#   define S_ISLNK(m) (((m) & S_IFMT) == S_IFLNK)
-#  else
-#   define S_ISLNK(m) 0
-#  endif
-# endif
-
-# ifndef HAVE_DECL_FREE
-"this configure-time declaration test was not run"
-# endif
-# if !HAVE_DECL_FREE
-void free ();
-# endif
-
-char *xmalloc ();
+# include "stat-macros.h"
+# include "xalloc.h"
 
 /* lstat works differently on Linux and Solaris systems.  POSIX (see
    `pathname resolution' in the glossary) requires that programs like `ls'
