92b20d96188a33efa8fe6ac713edbc2e715f2147
Derek R. Price <derek@ximbiot.com>
2004-10-22 01:28:51 +0000

    * error.c, error.h: Update from GNULIB.
    CVS-ID: lib/ChangeLog 1.358
    CVS-ID: lib/error.c 1.6
    CVS-ID: lib/error.h 1.5

--- lib/error.h
+++ lib/error.h
@@ -1,5 +1,6 @@
-/* error.h -- declaration for error-reporting function
-   Copyright (C) 1995 Software Foundation, Inc.
+/* Declaration for error-reporting function
+   Copyright (C) 1995, 1996, 1997, 2003 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -9,41 +10,57 @@
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.  */
+   GNU General Public License for more details.
 
-#ifndef ERROR_H
-#define ERROR_H
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-/* Add GNU attribute suppport.  */
+#ifndef _ERROR_H
+#define _ERROR_H 1
+
 #ifndef __attribute__
 /* This feature is available in gcc versions 2.5 and later.  */
-# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5)
 #  define __attribute__(Spec) /* empty */
-# else
-#   if __GNUC__ == 2 && __GNUC_MINOR__ < 96
-#    define __pure__	/* empty */
-#   endif
-#   if __GNUC__ < 3
-#    define __malloc__	/* empty */
-#   endif
 # endif
 /* The __-protected variants of `format' and `printf' attributes
    are accepted by gcc versions 2.6.4 (effectively 2.7) and later.  */
 # if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
-#  define __const__	const
-#  define __format__	format
-#  define __noreturn__	noreturn
-#  define __printf__	printf
+#  define __format__ format
+#  define __printf__ printf
 # endif
 #endif
 
-void error (int, int, const char *, ...)
-  __attribute__ ((__format__ (__printf__, 3, 4)));
+#ifdef	__cplusplus
+extern "C" {
+#endif
 
-/* If non-zero, error will use the CVS protocol to report error
-   messages.  This will only be set in the CVS server parent process;
-   most other code is run via do_cvs_command, which forks off a child
-   process and packages up its stderr in the protocol.  */
-extern int error_use_protocol;
+/* Print a message with `fprintf (stderr, FORMAT, ...)';
+   if ERRNUM is nonzero, follow it with ": " and strerror (ERRNUM).
+   If STATUS is nonzero, terminate the program with `exit (STATUS)'.  */
 
-#endif /* ERROR_H */
+extern void error (int __status, int __errnum, const char *__format, ...)
+     __attribute__ ((__format__ (__printf__, 3, 4)));
+
+extern void error_at_line (int __status, int __errnum, const char *__fname,
+			   unsigned int __lineno, const char *__format, ...)
+     __attribute__ ((__format__ (__printf__, 5, 6)));
+
+/* If NULL, error will flush stdout, then print on stderr the program
+   name, a colon and a space.  Otherwise, error will call this
+   function without parameters instead.  */
+extern void (*error_print_progname) (void);
+
+/* This variable is incremented each time `error' is called.  */
+extern unsigned int error_message_count;
+
+/* Sometimes we want to have at most one error per line.  This
+   variable controls whether this mode is selected or not.  */
+extern int error_one_per_line;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* error.h */
