0b5f00db0d9de4ff3c2dba149b5b1807c9065d56
Derek R. Price <derek@ximbiot.com>
2004-10-22 01:03:52 +0000

    * Makefile.am (libcvs_a_SOURCES): Move yesno to its own section. * rpmatch.c, yesno.h: New files from GNULIB. * yesno.c: Update from GNULIB.
    CVS-ID: lib/ChangeLog 1.356
    CVS-ID: lib/Makefile.am 1.75
    CVS-ID: lib/Makefile.gnulib 1.34
    CVS-ID: lib/Makefile.in 1.151
    CVS-ID: lib/rpmatch.c 1.1
    CVS-ID: lib/yesno.c 1.4
    CVS-ID: lib/yesno.h 1.1

--- lib/yesno.c
+++ lib/yesno.c
@@ -1,5 +1,5 @@
 /* yesno.c -- read a yes/no response from stdin
-   Copyright (C) 1990 Free Software Foundation, Inc.
+   Copyright (C) 1990, 1998, 2001, 2003, 2004 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -9,30 +9,48 @@
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.  */
+   GNU General Public License for more details.
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#if HAVE_CONFIG_H
+# include <config.h>
 #endif
 
+#include "yesno.h"
+
+#include <ctype.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+#if USE_UNLOCKED_IO
+# include "unlocked-io.h"
+#endif
 
 /* Read one line from standard input
    and return nonzero if that line begins with y or Y,
    otherwise return 0. */
 
-int
-yesno ()
+extern int rpmatch (char const *response);
+
+bool
+yesno (void)
 {
+  /* We make some assumptions here:
+     a) leading white space in the response are not vital
+     b) the first 128 characters of the answer are enough (the rest can
+	be ignored)
+     I cannot think for a situation where this is not ok.  --drepper@gnu  */
+  char buf[128];
+  int len = 0;
   int c;
-  int rv;
 
-  fflush (stderr);
-  fflush (stdout);
-  c = getchar ();
-  rv = (c == 'y') || (c == 'Y');
-  while (c != EOF && c != '\n')
-    c = getchar ();
+  while ((c = getchar ()) != EOF && c != '\n')
+    if ((len > 0 && len < 127) || (len == 0 && !isspace (c)))
+      buf[len++] = c;
+  buf[len] = '\0';
 
-  return rv;
+  return rpmatch (buf) == 1;
 }
