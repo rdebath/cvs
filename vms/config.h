adafc3fac4cbf419c075c59d5e0ef8dc0c12ed99
Derek R. Price <derek@ximbiot.com>
2005-04-18 19:44:38 +0000

    Update for 1.12.12.1
    CVS-ID: doc/stamp-1 1.65
    CVS-ID: doc/stamp-vti 1.142
    CVS-ID: doc/version-client.texi 1.65
    CVS-ID: doc/version.texi 1.143
    CVS-ID: vms/config.h 1.33
    CVS-ID: windows-NT/config.h 1.133
    CVS-ID: NEWS 1.312
    CVS-ID: configure 1.382
    CVS-ID: configure.in 1.349

--- vms/config.h
+++ vms/config.h
@@ -9,7 +9,7 @@
               June 1995 - <benjamin@cyclic.com> */
 
 /* The following macro are defined by running ./configure under UNIX OSs.  */
-#define PACKAGE_STRING "Concurrent Versions System (CVS) 1.12.12"
+#define PACKAGE_STRING "Concurrent Versions System (CVS) 1.12.12.1"
 
 /* We only want to build the client */
 #define CLIENT_SUPPORT 1
