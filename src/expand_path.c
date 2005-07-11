2be2a83c9706ad4404cb8d4af7623ae6a0a3edf8
Derek R. Price <derek@ximbiot.com>
2005-07-11 16:09:17 +0000

    Merge changes from 1.11.x.
    CVS-ID: contrib/ChangeLog 1.152
    CVS-ID: contrib/clmerge.in 1.2
    CVS-ID: contrib/cln_hist.in 1.2
    CVS-ID: contrib/commit_prep.in 1.4
    CVS-ID: contrib/cvs2vendor.sh 1.4
    CVS-ID: contrib/cvs_acls.in 1.8
    CVS-ID: contrib/debug_check_log.sh 1.3
    CVS-ID: contrib/descend.sh 1.3
    CVS-ID: contrib/log.in 1.4
    CVS-ID: contrib/log_accum.in 1.7
    CVS-ID: contrib/mfpipe.in 1.3
    CVS-ID: contrib/rcs-to-cvs.sh 1.5
    CVS-ID: contrib/rcs2log.sh 1.10
    CVS-ID: contrib/rcs2sccs.sh 1.5
    CVS-ID: contrib/rcslock.in 1.3
    CVS-ID: contrib/sandbox_status.sh 1.2
    CVS-ID: contrib/sccs2rcs.in 1.8
    CVS-ID: src/ChangeLog 1.3216
    CVS-ID: src/buffer.c 1.59
    CVS-ID: src/buffer.h 1.25
    CVS-ID: src/client.h 1.57
    CVS-ID: src/expand_path.c 1.35
    CVS-ID: src/history.c 1.93
    CVS-ID: src/myndbm.h 1.7
    CVS-ID: src/release.c 1.69
    CVS-ID: windows-NT/ChangeLog 1.319
    CVS-ID: windows-NT/mkdir.c 1.3
    CVS-ID: windows-NT/startserver.c 1.10
    CVS-ID: windows-NT/woe32.c 1.10
    CVS-ID: ChangeLog 1.1231
    CVS-ID: FAQ 1.26
    CVS-ID: HACKING 1.43
    CVS-ID: INSTALL-CVS 1.3

--- src/expand_path.c
+++ src/expand_path.c
@@ -1,5 +1,15 @@
 /* expand_path.c -- expand environmental variables in passed in string
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * The main routine is expand_path(), it is the routine that handles
  * the '~' character in four forms: 
  *     ~name
--- src/history.c
+++ src/history.c
@@ -1,9 +1,15 @@
-/*
+/* This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- *    You may distribute under the terms of the GNU General Public License
- *    as specified in the README file that comes with the CVS 1.0 kit.
- *
- * **************** History of Users and Module ****************
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* **************** History of Users and Module ****************
  *
  * LOGGING:  Append record to "${CVSROOT}/CVSROOTADM/CVSROOTADM_HISTORY".
  *
