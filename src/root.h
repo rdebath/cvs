300f4ea153d3a46b4c5ea63435a274b14808736b
Derek R. Price <derek@ximbiot.com>
2005-09-04 06:01:27 +0000

    * root.h (cvsroot_t->isremote): Update comment.
    CVS-ID: src/ChangeLog 1.3235
    CVS-ID: src/root.h 1.21

--- src/root.h
+++ src/root.h
@@ -31,7 +31,7 @@ typedef struct cvsroot_s {
     char *original;		/* The complete source CVSroot string. */
     CVSmethod method;		/* One of the enum values above. */
     char *directory;		/* The directory name. */
-    bool isremote;		/* Nonzero if we are doing remote access. */
+    bool isremote;		/* True if we are doing remote access. */
 /* The following is required for servers now to allow Redirects to be sent
  * for remote roots when client support is disabled.
  */
