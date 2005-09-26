7dec2dd7a46e95190f47f22c35c5deda5671f1c2
Conrad T. Pino <Conrad@Pino.com>
2005-09-26 03:02:13 +0000

    * socket.h: Guard against multiple includes with SYS_SOCKET_H macro. Change "#define WIN32_LEAN_AND_MEAN 1" to "#define WIN32_LEAN_AND_MEAN".
    CVS-ID: windows-NT/sys/ChangeLog 1.4
    CVS-ID: windows-NT/sys/socket.h 1.2

--- windows-NT/sys/socket.h
+++ windows-NT/sys/socket.h
@@ -1,4 +1,9 @@
-#define WIN32_LEAN_AND_MEAN 1
+#ifndef SYS_SOCKET_H
+#define SYS_SOCKET_H
+
+#define WIN32_LEAN_AND_MEAN
 #include <winsock2.h>
 
 typedef size_t socklen_t;
+
+#endif /* SYS_SOCKET_H */
