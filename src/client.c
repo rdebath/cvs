5ad5e26156f610ff83acae055aff1fdfd02d9cd5
Derek R. Price <derek@ximbiot.com>
2007-06-19 03:09:23 +0000

    Merge changes from 1.11.x.
    CVS-ID: src/ChangeLog 1.3517
    CVS-ID: src/client.c 1.462
    CVS-ID: ChangeLog 1.1353
    CVS-ID: NEWS 1.372

--- src/client.c
+++ src/client.c
@@ -3336,7 +3336,8 @@ send_repository (const char *dir, const char *repos, const char *update_dir)
 	send_to_server (repos, 0);
     send_to_server ("\012", 1);
 
-    if (supported_request ("Static-directory"))
+    if (strcmp (cvs_cmd_name, "import")
+	&& supported_request ("Static-directory"))
     {
 	adm_name[0] = '\0';
 	if (dir[0] != '\0')
@@ -3350,7 +3351,8 @@ send_repository (const char *dir, const char *repos, const char *update_dir)
 	    send_to_server ("Static-directory\012", 0);
 	}
     }
-    if (supported_request ("Sticky"))
+    if (strcmp (cvs_cmd_name, "import")
+	&& supported_request ("Sticky"))
     {
 	FILE *f;
 	if (dir[0] == '\0')
